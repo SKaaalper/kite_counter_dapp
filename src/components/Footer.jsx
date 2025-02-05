383b50a59d4ba9f0fbc25f2b61c6b368daedd0f4
Vikas Pandey <vikpande@gmail.com>
2025-02-05 15:50:41 +0530

    Create counter dapp
    Create counter dapp

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      ©2025 Kite AI - Counter Dapp. Powered by Kite AI
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../assets/network_logo.svg";
+export default function Header({ wallet, connectWallet, disconnectWallet }) {
+  return (
+    <header className="headerwrap w-full bg-white text-white py-4 px-6 flex flex-wrap justify-between items-center border-gray-200 border-b">
+      <h1 className="logowrap"><img src={logo} />Counter Dapp</h1>
+
+      <div className="flex items-center space-x-4">
+        {wallet && (
+          <span className="text-[#0077FF] font-medium border border-[#A9D1FF] rounded-md transition-all duration-300 flex items-center justify-center bg-gradient-to-t from-[#E1EFFF] to-[#EFF6FF] hover:from-[#0077FF] hover:to-[#0077FF] hover:text-white    text-md py-2 px-5 cursor-pointer ">
+            Connected: {wallet.slice(0, 6)}...{wallet.slice(-4)}
+          </span>
+        )}
+
+        <a
+          href="https://github.com/YourGithubRepo"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-[#0077FF] font-medium border border-[#A9D1FF] rounded-md transition-all duration-300 flex items-center justify-center bg-gradient-to-t from-[#E1EFFF] to-[#EFF6FF] hover:from-[#0077FF] hover:to-[#0077FF] hover:text-white  text-md py-2 px-5 cursor-pointer"
+        >
+          GitHub Repo
+        </a>
+      </div>
+    </header>
+  );
+}
